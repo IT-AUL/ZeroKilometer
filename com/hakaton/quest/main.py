556c2d1cf1140b1cab4b02480077ef85a0fb3709
DanisGaleev <galeevdanisgdr@gmail.com>
2024-05-11 23:29:20 +0300

    [*] Cleaned code [+] Requirements

--- com/hakaton/quest/main.py
+++ com/hakaton/quest/main.py
@@ -6,7 +6,7 @@ from aiogram import Bot, Dispatcher, F, types
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, CallbackQuery
 
 from dialogue import Translate
 from npc_manager import ask_question
@@ -32,7 +32,7 @@ async def start_quest(message: Message) -> None:
     if message.from_user.id not in players.keys():
         players[user_id] = Player()
         quest_managers[user_id] = QuestManager(player=players[user_id])
-        await message.answer(text="Глава: " + quest_managers[user_id].current_chapter.title)
+        await message.answer(text=f"Глава: <b>{quest_managers[user_id].current_chapter.title}</b>")
     is_talking_with_npc[user_id] = False
     quest_description, markup = quest_managers[user_id].get_quest_desc_and_choices()
     await message.answer(text=quest_description, reply_markup=markup)
@@ -47,10 +47,10 @@ async def check_location(message: Message) -> None:  # check if player near righ
         players[user_id].changed_location = False
         quest_description, markup = user.get_quest_desc_and_choices()
 
-        await message.answer(text="Глава: " + user.current_chapter.title, reply_markup=ReplyKeyboardRemove())
+        await message.answer(text=f"Глава: <b>{user.current_chapter.title}</b>", reply_markup=ReplyKeyboardRemove())
         await message.answer(text=quest_description, reply_markup=markup)
     elif geodesic(_location, user.current_chapter.geo_position).meters > 50:
-        await message.answer("Вы слишком далеко. Подойдите не меньше, чем на 50 метров.")
+        await message.answer("<i>Вы слишком далеко. Подойдите не меньше, чем на 50 метров.</i>")
 
 
 @dp.message()
@@ -61,42 +61,45 @@ async def managing_player_responses(message: Message):
         await message.answer(text=answer)
 
 
-@dp.callback_query(F.data != "ask_question")
+@dp.callback_query(F.data.endswith("ask"))
+async def handle_ask_question(callback: CallbackQuery):
+    user_id = callback.from_user.id
+    is_talking_with_npc[user_id] = True
+    await callback.message.edit_reply_markup(reply_markup=None)
+    await callback.message.answer(text="Ну, спрашивай")
+
+
+@dp.callback_query()
 async def apply_choice(callback: types.CallbackQuery):
     user_id = callback.from_user.id
     user = quest_managers[user_id]
-    if callback.data.endswith('ask'):
-        is_talking_with_npc[user_id] = True
-        await callback.message.edit_reply_markup(reply_markup=None)
-        await callback.message.answer(text="Ну, спрашивай")
+    for choice in user.current_quest.choices:
+        is_talking_with_npc[user_id] = False
+
+        # i compare unique combination of current chapter, quest and choice id with current choice
+        compare_data = user.current_chapter_id + ";" + user.current_quest_id + ";" + choice.choice_id
+        if compare_data == callback.data:
+            if choice.to_quest.startswith("ch"):
+                players[user_id].changed_location = True
+
+                user.current_chapter_id = choice.to_quest
+                user.current_chapter = user.chapters[user.current_chapter_id]
+                user.current_quest_id = "q0"
+                user.current_quest = user.current_chapter.quests[user.current_quest_id]
+
+                await callback.message.answer(text="<b>Новая локация</b>", reply_markup=next_chapter_button_markup)
+                await callback.message.edit_reply_markup(reply_markup=None)
+            else:
+                user.current_quest_id = choice.to_quest
+                quest_description, markup = user.get_quest_desc_and_choices()
+                players[user_id].apply_changes(**choice.result)
+
+                await callback.message.edit_reply_markup(reply_markup=None)
+                await callback.message.answer(text=choice.text)
+                await callback.message.answer(text=quest_description, reply_markup=markup)
+            break
     else:
-        for choice in user.current_quest.choices:
-            is_talking_with_npc[user_id] = False
-
-            # i compare unique combination of current chapter, quest and choice id with current choice
-            compare_data = user.current_chapter_id + ";" + user.current_quest_id + ";" + choice.choice_id
-            if compare_data == callback.data:
-                if choice.to_quest.startswith("ch"):
-                    players[user_id].changed_location = True
-
-                    user.current_chapter_id = choice.to_quest
-                    user.current_chapter = user.chapters[user.current_chapter_id]
-                    user.current_quest_id = "q0"
-                    user.current_quest = user.current_chapter.quests[user.current_quest_id]
-
-                    await callback.message.answer(text="Новая локация", reply_markup=next_chapter_button_markup)
-                    await callback.message.edit_reply_markup(reply_markup=None)
-                else:
-                    user.current_quest_id = choice.to_quest
-                    quest_description, markup = user.get_quest_desc_and_choices()
-                    players[user_id].apply_changes(**choice.result)
-
-                    await callback.message.edit_reply_markup(reply_markup=None)
-                    await callback.message.answer(text=choice.text)
-                    await callback.message.answer(text=quest_description, reply_markup=markup)
-                break
-        else:
-            await callback.message.delete()
+        await callback.message.delete()
 
 
 async def main() -> None:
