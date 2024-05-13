4c1348b2b647b9c8b2adf694131bc0d0c33e6a09
DanisGaleev <galeevdanisgdr@gmail.com>
2024-05-13 21:10:02 +0300

    [+] Map

--- com/hakaton/quest/main.py
+++ com/hakaton/quest/main.py
@@ -7,7 +7,7 @@ import sys
 from aiogram import Bot, Dispatcher, F, types
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import (
     Message,
     ReplyKeyboardMarkup,
@@ -31,6 +31,8 @@ quest_managers = {}
 is_talking_with_npc = {}
 translate = Translate()
 
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
 next_chapter_button_markup = ReplyKeyboardMarkup(
     keyboard=[[KeyboardButton(text="Двигаться дальше", request_location=True)]])
 
@@ -47,9 +49,21 @@ async def start_quest(message: Message) -> None:
     await message.answer(text=quest_description, reply_markup=markup)
 
 
+@dp.message(Command("clear"))
+async def clear(message: Message) -> None:
+    user_id = message.from_user.id
+    players[user_id] = Player()
+    quest_managers[user_id] = QuestManager(player=players[user_id])
+    await message.answer(text=f"Глава: <b>{quest_managers[user_id].current_chapter.title}</b>")
+    is_talking_with_npc[user_id] = False
+    quest_description, markup = quest_managers[user_id].get_quest_desc_and_choices()
+    await message.answer(text=quest_description, reply_markup=markup)
+
+
 @dp.message(F.location)
 async def check_location(message: Message) -> None:  # check if player near right place
     _location = (message.location.latitude, message.location.longitude)
+    print(message.from_user.id, _location)
     user_id = message.from_user.id
     user = quest_managers[user_id]
     if players[user_id].changed_location and geodesic(_location, user.current_chapter.geo_position).meters <= 50:
@@ -59,7 +73,10 @@ async def check_location(message: Message) -> None:  # check if player near righ
         await message.answer(text=f"Глава: <b>{user.current_chapter.title}</b>", reply_markup=ReplyKeyboardRemove())
         await message.answer(text=quest_description, reply_markup=markup)
     elif geodesic(_location, user.current_chapter.geo_position).meters > 50:
-        await message.answer("<i>Вы слишком далеко. Подойдите не меньше, чем на 50 метров.</i>")
+        await bot.send_venue(message.chat.id, latitude=user.current_chapter.geo_position[0],
+                             longitude=user.current_chapter.geo_position[1], title="Вы слишком далеко.",
+                             address="Следующая точка здесь.")
+        await message.answer("<i>Подойдите не меньше, чем на 50 метров.</i>")
 
 
 @dp.message()
@@ -112,7 +129,6 @@ async def apply_choice(callback: types.CallbackQuery):
 
 
 async def main() -> None:
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await dp.start_polling(bot)
 
 
